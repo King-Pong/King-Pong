8c830982836a5eba9081c9fd4c8179fd17ab9fdc
Ikenna <ikennaugwuh@gmail.com>
2016-07-28 17:46:38 -0400

    Updated server constant with a route to store players on the class server.

--- src/js/utilities/server.constant.js
+++ src/js/utilities/server.constant.js
@@ -1,5 +1,4 @@
-const serverConstant = {
+export default serverConstant = {
     URL: 'https://hackathon-team-brit.herokuapp.com/',
+    PLAYERS: 'http://class-server.herokuapp.com/collections/king-pong-players/'
 };
-
-export default serverConstant;
