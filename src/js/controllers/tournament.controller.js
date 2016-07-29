1e12428f9ef28f0abcd22c7498da4bdb92878e2e
Ikenna <ikennaugwuh@gmail.com>
2016-07-29 12:05:40 -0400

    Added some functionality for adding players to a tournament.

--- src/js/controllers/tournament.controller.js
+++ src/js/controllers/tournament.controller.js
@@ -7,6 +7,9 @@ export default function Tournament (MatchService, BackendService, UserService, $
     vm.start = start;
     vm.players = [];
     vm.playerList = getPlayerList() || [];
+    vm.setTourneySize = setSize;
+    vm.playerResults = [];
+    vm.validate = validatePlayer;
 
     function createPlayer(player){
         if (player.nickname = prompt("Please provide a player nickname:"))
@@ -16,19 +19,21 @@ export default function Tournament (MatchService, BackendService, UserService, $
             }, err => { console.log(err); });
     }
 
-    function validatePlayer(player){
-        if (vm.playerList){
-            vm.playerResults = [];
-            vm.playerList.forEach(p => {
-                if (p.fullname.toLowerCase().includes(player.fullname.toLowerCase()) ||
-                p.nickname.toLowerCase().includes(player.fullname.toLowerCase()))
-                    vm.playerResults.push(p);
-            });
-        }
+    function validatePlayer(name){
+        vm.playerResults = [];
+        if (name = name.toLowerCase()) vm.playerList.forEach(p => {
+            if (p.fullname.toLowerCase().includes(name) ||
+            p.nickname.toLowerCase().includes(name))
+                vm.playerResults.push(p);
+        });
     }
 
     function addPlayer(player){
+        if (typeof player == 'number'){
+            player = vm.playerList.filter(p => p.id === player)[0];
+        }
         vm.players.push(player);
+        console.log(vm.players);
     }
 
      function start(){
@@ -45,6 +50,10 @@ export default function Tournament (MatchService, BackendService, UserService, $
              vm.playerList = resp.data.players;
              console.log(vm.playerList);
          });;
+     }
+
+     function setSize(size){
+         vm.size = size;
      }
 }
 Tournament.$inject = ['MatchService', 'BackendService', 'UserService', '$state'];
